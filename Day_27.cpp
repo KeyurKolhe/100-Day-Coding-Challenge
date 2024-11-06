c8926e7e03543aa29b2f34cfdecbc8aebf5014db
Keyur Kolhe <146522383+KeyurKolhe@users.noreply.github.com>
2024-11-06 17:14:46 +0530

    Create Day_27.cpp

---
+++ Day_27.cpp
@@ -0,0 +1,12 @@
+//Day 2 coding Statement : Write a program to identify if the character is an alphabet or not.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter a Number : ";
+    cin >> num;
+    cout << "Double is : " << (num<<1);
+    return 0;
+}
