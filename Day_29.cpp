2300dca7d59f64df5256870a9ea38a09736fbf7c
Keyur Kolhe <146522383+KeyurKolhe@users.noreply.github.com>
2024-11-08 21:02:52 +0530

    Create Day_29.cpp

---
+++ Day_29.cpp
@@ -0,0 +1,14 @@
+//Day 29 coding Statement : Write a Program to concatenate a string.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str,str1;
+    cout << "Enter First String : \n";
+    cin >> str;
+    cout << "Enter Second String : \n";
+    cin >> str1;
+    cout << str << str1;
+    return 0;
+}
