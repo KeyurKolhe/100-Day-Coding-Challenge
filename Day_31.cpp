47949161504c8b684016427f6e1e0dcf98e721ac
Keyur Kolhe <146522383+KeyurKolhe@users.noreply.github.com>
2024-11-12 18:02:00 +0530

    Create Day_31.cpp

---
+++ Day_31.cpp
@@ -0,0 +1,20 @@
+//Day 31 coding Statement : Write a Program to Toggle each character in a string
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    char str[100];
+    cout << "Enter a String : \n";
+    cin.getline(str, 100);
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if(islower(str[i]))
+            str[i] = toupper(str[i]);
+        else
+            str[i] = tolower(str[i]);
+    }
+    cout << "Toggled Output :" << str;
+}
